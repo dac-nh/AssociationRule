47bd0fe68e53f242397fd10b53f8ea1bf2348b12
Dark Wolf <dachuu25@gmail.com>
2017-03-05 10:18:08 +0700

    done

--- src/Main/SourceMain.java
+++ src/Main/SourceMain.java
@@ -17,17 +17,24 @@ public class SourceMain {
     }
 
     private static void demo() {
-        AprioriFrequentItemsetGenerator<String> generator =
-                new AprioriFrequentItemsetGenerator<>();
+        double minSup = 0.02, minConf = 0.4; // minimum Support and Minimum Confidence
+        AprioriFrequentItemsetGenerator<String> generator = new AprioriFrequentItemsetGenerator<>();
 
+        // Input from mysql: test, user=root, pass="",table data_for_association_rule
+        List<Set<String>> itemsetList = new ArrayList<>(); // New Array list
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM data_for_association_rule");
-            System.out.print(rs);
+            while (rs.next()) {
+                String[] tempArrayListStr = rs.getString(2).split(","); // split by ,
+                itemsetList.add(new HashSet<>(Arrays.asList(tempArrayListStr)));
+            }
         } catch (SQLException | IllegalAccessException | ClassNotFoundException | InstantiationException e) {
             e.printStackTrace();
+            System.out.println("Have some problem in input");
+            return;
         }
 
 //        List<Set<String>> itemsetList = new ArrayList<>();
@@ -44,40 +51,40 @@ public class SourceMain {
 //        itemsetList.add(new HashSet<>(Arrays.asList("a", "b", "d")));
 //        itemsetList.add(new HashSet<>(Arrays.asList("b", "c", "e")));
 //
-//        long startTime = System.nanoTime();
-//        FrequentItemsetData<String> data = generator.generate(itemsetList, 0.02);
-//        long endTime = System.nanoTime();
-//
-//        int i = 1;
-//
-//        System.out.println("--- Frequent itemsets ---");
-//
-//        for (Set<String> itemset : data.getFrequentItemsetList()) {
-//            System.out.printf("%2d: %9s, support: %1.1f\n",
-//                    i++,
-//                    itemset,
-//                    data.getSupport(itemset));
-//        }
-//
-//        System.out.printf("Mined frequent itemset in %d milliseconds.\n",
-//                (endTime - startTime) / 1_000_000);
-//
-//        startTime = System.nanoTime();
-//        List<AssociationRule<String>> associationRuleList =
-//                new AssociationRuleGenerator<String>()
-//                        .mineAssociationRules(data, 0.4);
-//        endTime = System.nanoTime();
-//
-//        i = 1;
-//
-//        System.out.println();
-//        System.out.println("--- Association rules ---");
-//
-//        for (AssociationRule<String> rule : associationRuleList) {
-//            System.out.printf("%2d: %s\n", i++, rule);
-//        }
-//
-//        System.out.printf("Mined association rules in %d milliseconds.\n",
-//                (endTime - startTime) / 1_000_000);
+        long startTime = System.nanoTime();
+        FrequentItemsetData<String> data = generator.generate(itemsetList, minSup);
+        long endTime = System.nanoTime();
+
+        int i = 1;
+
+        System.out.println("--- Frequent itemsets ---");
+
+        for (Set<String> itemset : data.getFrequentItemsetList()) {
+            System.out.printf("%2d: %9s, support: %1.1f\n",
+                    i++,
+                    itemset,
+                    data.getSupport(itemset));
+        }
+
+        System.out.printf("Mined frequent itemset in %d milliseconds.\n",
+                (endTime - startTime) / 1_000_000);
+
+        startTime = System.nanoTime();
+        List<AssociationRule<String>> associationRuleList =
+                new AssociationRuleGenerator<String>()
+                        .mineAssociationRules(data, minConf);
+        endTime = System.nanoTime();
+
+        i = 1;
+
+        System.out.println();
+        System.out.println("--- Association rules ---");
+
+        for (AssociationRule<String> rule : associationRuleList) {
+            System.out.printf("%2d: %s\n", i++, rule);
+        }
+
+        System.out.printf("Mined association rules in %d milliseconds.\n",
+                (endTime - startTime) / 1_000_000);
     }
 }
